6eaf050acbd4e630bc06a1b40105a74bd5e53034
chirs_ren@163.com <chirs_ren@163.com>
2025-02-21 14:54:06 +0800

    Initial commit

--- src/layout/footer/index.vue
+++ src/layout/footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    Copyright © 2012-2025 Python知识分享网 版权所有&nbsp;&nbsp;<a href="http://www.python222.com" target="_blank">www.python222.com</a>
+    Copyright © 2012-2025 chirs_ren 版权所有&nbsp;&nbsp;<a href="http://www.baidu.com" target="_blank">www.baidu.com</a>
   </div>
 </template>
 
